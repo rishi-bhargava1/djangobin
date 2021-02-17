5261cea691cf4a8ed794ef0d35ca55bc44d1d83e
Rishi_bhargava <rishib428@gmail.com>
2021-02-17 08:44:40 +0530

    djangobin: urls, views update.

--- django_project/djangobin/urls.py
+++ django_project/djangobin/urls.py
@@ -6,4 +6,8 @@ app_name = 'djangobin'
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^user/(?P<username>[A-Za-z0-9]+)/$', views.profile, name='profile'),
+    url('^trending/$', views.trending_snippets, name='trending_snippets'),
+    url('^trending/(?P<language_slug>[\w]+)/$', views.trending_snippets, name='trending_snippets'),
+    url('^(?P<snippet_slug>[\d]+)/$', views.snippet_detail, name='snippet_detail'),
+    url('^tag/(?P<tag>[\w-]+)/$', views.tag_list, name='tag_list'),
 ]
--- django_project/djangobin/views.py
+++ django_project/djangobin/views.py
@@ -9,6 +9,18 @@ def index(request):
     return HttpResponse('<p>Index Page</p>')
 
 
+def trending_snippets(request, language_slug=''):
+    return HttpResponse("trending {} snippets".format(language_slug if language_slug else ""))
+
+
+def snippet_detail(request, snippet_slug):
+    return HttpResponse('viewing snippet #{}'.format(snippet_slug))
+
+
+def tag_list(request, tag):
+    return HttpResponse('viewing tag #{}'.format(tag))
+
+
 def profile(request, username):
     html = f"<html><body>Profile Page of: {username}</body></html>"
     return HttpResponse(html)
